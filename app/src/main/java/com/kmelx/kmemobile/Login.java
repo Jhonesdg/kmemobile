f1a8ba69e0bfa581343e98a7e293e436a1bc9221
Jonathan Dorado <jonathan@MacBook-Pro-de-Jonathan.local>
2016-04-26 23:00:19 -0500

    [KmeMobile] first commit app

---
+++ app/src/main/java/com/kmelx/kmemobile/Login.java
@@ -0,0 +1,17 @@
+package com.kmelx.kmemobile;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import com.facebook.FacebookSdk;
+import com.facebook.appevents.AppEventsLogger;
+
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        FacebookSdk.sdkInitialize(getApplicationContext());
+        AppEventsLogger.activateApp(this);
+        setContentView(R.layout.activity_login);
+    }
+}
