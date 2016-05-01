97bd3c44c80f329bac7700cfa651e1b9ff4acb80
Jonathan Dorado <jonathan@MacBook-Pro-de-Jonathan.local>
2016-05-01 15:11:04 -0500

    [KmeMobile] login and signup design

--- app/src/main/java/com/kmelx/kmemobile/Login.java
+++ app/src/main/java/com/kmelx/kmemobile/Login.java
@@ -1,7 +1,10 @@
 package com.kmelx.kmemobile;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
 import com.facebook.FacebookSdk;
 import com.facebook.appevents.AppEventsLogger;
 
@@ -13,5 +16,10 @@ public class Login extends AppCompatActivity {
         FacebookSdk.sdkInitialize(getApplicationContext());
         AppEventsLogger.activateApp(this);
         setContentView(R.layout.activity_login);
+    }
+    public void Signup(View v){
+        Intent intento = new Intent (Login.this, SignUp.class);
+        startActivity(intento);
+
     }
 }
