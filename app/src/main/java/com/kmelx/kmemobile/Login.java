3f1c5fcad3c6239e1972c723694da137a5df1b89
Jonathan Dorado <jonathan@MacBook-Pro-de-Jonathan.local>
2016-05-04 23:50:47 -0500

    Design luz

--- app/src/main/java/com/kmelx/kmemobile/Login.java
+++ app/src/main/java/com/kmelx/kmemobile/Login.java
@@ -3,19 +3,22 @@ package com.kmelx.kmemobile;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.Window;
 
 import com.facebook.FacebookSdk;
 import com.facebook.appevents.AppEventsLogger;
 
 public class Login extends AppCompatActivity {
-
+    Fblogin fblogin;
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate( Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        FacebookSdk.sdkInitialize(getApplicationContext());
         AppEventsLogger.activateApp(this);
         setContentView(R.layout.activity_login);
+
+
     }
     public void Signup(View v){
         Intent intento = new Intent (Login.this, SignUp.class);
---
+++ app/src/main/java/com/kmelx/kmemobile/Myapplication.java
@@ -0,0 +1,41 @@
+package com.kmelx.kmemobile;
+
+
+import android.app.Application;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
+import android.util.Base64;
+import android.util.Log;
+import com.facebook.FacebookSdk;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+/**
+ * Created by Windows on 26-03-2015.
+ */
+public class Myapplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        printKeyHash();
+    }
+
+    /**
+     * Call this method inside onCreate once to get your hash key
+     */
+    public void printKeyHash() {
+        try {
+            PackageInfo info = getPackageManager().getPackageInfo("com.kmelx.kmemobile", PackageManager.GET_SIGNATURES);
+            for (Signature signature : info.signatures) {
+                MessageDigest md = MessageDigest.getInstance("SHA");
+                md.update(signature.toByteArray());
+                Log.e("KMEMOBILELULITO", Base64.encodeToString(md.digest(), Base64.DEFAULT));
+            }
+        } catch (PackageManager.NameNotFoundException e) {
+
+        } catch (NoSuchAlgorithmException e) {
+
+        }
+    }
+}
