8fb8a98a1de275f29cf1859d4d1bb17f5e62d453
jonathan <jonathan.dorado@nuevosmedios.ws>
2016-05-10 00:58:10 -0500

    json

--- app/src/main/java/com/kmelx/kmemobile/Login.java
+++ app/src/main/java/com/kmelx/kmemobile/Login.java
@@ -1,25 +1,36 @@
 package com.kmelx.kmemobile;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.Window;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.SeekBar;
 import android.widget.Toast;
 
 import com.android.volley.AuthFailureError;
+import com.android.volley.DefaultRetryPolicy;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
+import com.android.volley.RetryPolicy;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.StringRequest;
 import com.facebook.FacebookSdk;
 import com.facebook.appevents.AppEventsLogger;
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -28,63 +39,92 @@ public class Login extends AppCompatActivity {
     Button btnLogin;
     EditText username;
     EditText passwd;
-    JsonObjectRequest array;
+    ProgressBar progressBar;
     RequestQueue mRequestQueue;
+
     private final String url = "http://kmelx.com/api/login_kme/";
     private final String TAG = "PRUEBITA";
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
+
     @Override
-    protected void onCreate( Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         AppEventsLogger.activateApp(this);
         setContentView(R.layout.activity_login);
-        username= (EditText) findViewById(R.id.username);
-        passwd= (EditText) findViewById(R.id.pasword);
-        btnLogin= (Button) findViewById(R.id.btnlogin);
+        username = (EditText) findViewById(R.id.username);
+        passwd = (EditText) findViewById(R.id.pasword);
+        btnLogin = (Button) findViewById(R.id.btnlogin);
+        progressBar = (ProgressBar) findViewById(R.id.progress_bar);
+        progressBar.setVisibility(View.INVISIBLE);
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-            mRequestQueue= VolleySingleton.getInstance().getmRequestQueue();
-                StringRequest request = new StringRequest(Request.Method.POST, url, new Response.Listener<String>() {
+                mRequestQueue = VolleySingleton.getInstance().getmRequestQueue();
+                progressBar.setVisibility(View.VISIBLE);
+                JsonObjectRequest array = new JsonObjectRequest(Request.Method.POST, url, new Response.Listener<JSONObject>() {
                     @Override
-                    public void onResponse(String response) {
-                        String token= response;
-                        Toast.makeText(getApplicationContext(),response,Toast.LENGTH_SHORT).show();
+                    public void onResponse(JSONObject response) {
+                        progressBar.setVisibility(View.INVISIBLE);
+                        try {
+                            if (response.getString("status")=="true"){
+                                startActivity(new Intent(getApplicationContext(), Home.class));
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+
+                        Toast.makeText(getApplicationContext(), response.toString(), Toast.LENGTH_SHORT).show();
 
                     }
                 }, new Response.ErrorListener() {
                     @Override
                     public void onErrorResponse(VolleyError error) {
-
-                        Toast.makeText(getApplicationContext(),error.toString(),Toast.LENGTH_SHORT).show();
+                        progressBar.setVisibility(View.INVISIBLE);
+                        Toast.makeText(getApplicationContext(), error.toString(), Toast.LENGTH_SHORT).show();
                     }
-                }){
+                }) {
                     @Override
                     protected Map<String, String> getParams() throws AuthFailureError {
-                        Map<String,String> map = new HashMap<String,String>();
-                        map.put("username",username.getText().toString());
-                        map.put("password",passwd.getText().toString());
+                        Map<String, String> map = new HashMap<String, String>();
+                        map.put("username", username.getText().toString());
+                        map.put("password", passwd.getText().toString());
                         return map;
                     }
 
                     @Override
                     public Map<String, String> getHeaders() throws AuthFailureError {
-                        Map<String,String> params = new HashMap<String,String>();
-                        params.put("Content-Type","application/x-www-form-urlencoded");
+                        Map<String, String> params = new HashMap<String, String>();
+                        params.put("Content-Type", "application/x-www-form-urlencoded");
                         return params;
                     }
+
+                    @Override
+                    public RetryPolicy getRetryPolicy() {
+                        return new DefaultRetryPolicy(
+                                5000,
+                                DefaultRetryPolicy.DEFAULT_MAX_RETRIES,
+                                DefaultRetryPolicy.DEFAULT_BACKOFF_MULT
+                        );
+                    }
                 };
-                mRequestQueue.add(request);
+                mRequestQueue.add(array);
             }
         });
 
 
-
-
-
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
-    public void Signup(View v){
-        Intent intento = new Intent (Login.this, SignUp.class);
+
+    public void Signup(View v) {
+        Intent intento = new Intent(Login.this, SignUp.class);
         startActivity(intento);
 
     }
+
 }
